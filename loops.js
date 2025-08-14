a34a28e677ef4910a8cb202a5e4f2b6b84c21ab1
Vaibhav123-dotcom <143591012+Vaibhav123-dotcom@users.noreply.github.com>
2025-08-14 13:34:12 +0530

    updated

---
+++ loops.js
@@ -0,0 +1,97 @@
+// finbonacci series
+// let first = 0 ;
+// let second = 1 ;
+
+// console.log(first);
+// console.log(second)
+// for(let i=1 ; i<=5 ; i++){
+//     let sum = first + second ;
+//     console.log(sum)
+//     first = second ;
+//     second = sum
+// }
+
+// q-1  : 2, 1, (1/2), (1/4) ...
+// let a = 4 ;
+// let b = 2 ;
+// for(let i=1 ; i<=5 ; i++){
+//     let res = a / b ;
+//     console.log(res);
+//     a = res ;
+// }
+
+// op -    2
+//         1
+//         0.5
+//         0.25
+//         0.125
+
+// q-2 : 7, 10, 8, 11, 9, 12,...
+
+// let a = 7 , b = 10 ;
+// for(let i=1 ; i<=5 ; i++){
+//     console.log(a)
+//     console.log(b)
+//     a++;
+//     b++;
+// }
+
+// q-3 : 36, 34, 30, 28, 24 ....
+// let first = 36 ;
+// let second = 34 ;
+// console.log(first)
+// console.log(second)
+// for(let i= 1 ;i<=5 ; i++){
+//     let div = first - 6 ;
+//     let div2 = second - 6 ;
+//     console.log(div)
+//     console.log(div2)
+//     first = div ;
+//     second = div2 ;
+// }
+
+// q - 4 : 22, 21, 23, 22, 24, 23, ...
+// let a = 22;
+// let b = 21;
+// console.log(a);
+// console.log(b);
+// for (let i = 1; i <= 5; i++) {
+//   a++;
+//   b++;
+//   console.log(a);
+//   console.log(b);
+// }
+
+// q-5 : 53, 53, 40, 40, 27, 27, ...
+// let a = 53 ;
+// let b = 53 ;
+// console.log(a)
+// console.log(b)
+// for(let i = 1 ;i<=4 ; i++){
+//     let sub = a - 13 ;
+//     let sub2 = b - 13 ;
+//     console.log(sub)
+//     console.log(sub2)
+//     a = sub ;
+//     b = sub2
+// }
+
+// q-6 : 21, 9, 21, 11, 21, 13, 21, ...
+// let a = 21 ;
+// let b = 9 ;
+// for(let i = 1 ; i<=6 ; i++){
+//     console.log(a)
+//     console.log(b)
+//     let add = b + 2
+//     b = add
+// }
+
+// q-7 :  58, 52, 46, 40, 34, ... 
+
+// let a = 58 ;
+// console.log(a)
+// for(let i = 1 ; i<=7 ; i++){
+//     let res = a - 6;
+//     a = res ;
+//     console.log(res)
+// }
