60373837b7b3bc29fffe4bdd249b4d2028257eac
Vaibhav123-dotcom <143591012+Vaibhav123-dotcom@users.noreply.github.com>
2025-08-26 15:18:01 +0530

    added

---
+++ Array.js
@@ -0,0 +1,17 @@
+// write a program to find pair of a given value 
+
+// let target = 7 ;
+// let arr = [1,2,3,4,5,6];
+// let pair = [] ;
+// let time = 0;
+
+// for(let i=0 ; i<arr.length ; i++){
+//     time++
+//     for(let j=i+1 ; j<arr.length ; j++){
+//         if(arr[i]+arr[j]==target){
+//             pair.push([arr[i],arr[j]])
+//         }
+//     }
+// }
+// console.log(pair)
+// console.log("time" , time)
